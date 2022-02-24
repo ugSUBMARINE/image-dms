228baa60775851264a08da2830e51d242a56d6eb
gwirn <71886945+gwirn@users.noreply.github.com>
2022-02-24 08:23:36 +0100

    moved functions from d4batch_distance_functions.py

---
+++ d4_data.py
@@ -0,0 +1,354 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from d4_utils import aa_dict
+
+
+def read_and_process(path_to_file, variants, silent=True, remove_astrix=True):
+    """reads in the deep mutational scanning file and returns its data\n
+        input:
+            path_to_file: path to the tsv file\n
+            variants: how the variants' column in the file ins named\n
+            silent: if True doesn't print any stats/ information\n
+            remove_astrix: if True excludes nonsense mutations from the file\n
+        :returns
+            p_name: name of the protein\n
+            raw_data: data as pandas df\n"""
+    raw_data = pd.read_csv(path_to_file, delimiter="\t")
+    # getting the proteins name
+    if "/" in path_to_file:
+        p_name = path_to_file.strip().split("/")[-1].split(".")[0]
+    else:
+        p_name = path_to_file.strip().split(".")[0]
+    # some information about the file
+    if not silent:
+        print(" - ".join(raw_data.columns.tolist()))
+        print()
+        sc = 0
+        for i in list(raw_data):
+            if "score" in i:
+                sc += 1
+        if sc > 1:
+            print("*** multiple scores are given - choose appropriate one ***")
+        print("raw data length:", len(raw_data))
+        print("protein name:", p_name)
+    # checking each variant whether there is a mutation with an "*" and removing it
+    if remove_astrix:
+        rd_bool = []
+        for i in raw_data[variants]:
+            rd_bool += ["*" not in i]
+        raw_data = raw_data[rd_bool]
+        if not silent:
+            print("*** {} rows had to be excluded due to incompatibility ***".format(np.sum(np.invert(rd_bool))))
+    return p_name, raw_data
+
+
+def check_seq(raw_data_cs, name_cs, wt_seq_cs, variants_cs, save_fig=None, plot_fig=False, silent=True):
+    """checks whether the wild sequence and the sequence reconstructed from the raw data match and plot a histogram
+        which shows how often a certain residue was part of a mutation\n
+        input:
+            raw_data_cs: dms data as pd dataframe\n
+            name_cs: protein name\n
+            wt_seq_cs: wild type sequence as list eg ['A', 'V', 'L']\n
+            variants_cs: name of the variants column in the raw data file\n
+            save_fig: None doesn't safe the histogram anything else does\n
+            plot_fig: if True shows the histogram\n
+            silent: if True doesn't show stats during execution\n
+        :return
+            first_ind: starting int of the sequence\n
+            """
+    # all variants in a list
+    v = raw_data_cs[variants_cs].tolist()
+    # list of lists with original amino acid and its residue index in the sequence
+    # from all listed variations
+    pre_seq = []
+    for i in v:
+        vi = i.strip().split(",")
+        for j in vi:
+            pre_seq += [[j[0], j[1:-1]]]
+
+    pro_seq = np.unique(pre_seq, axis=0)
+    # list of lists with original amino acid and its residue index in the sequence
+    # only unique entries = reconstructed sequence
+    pro_seq_sorted = pro_seq[np.argsort(pro_seq[:, 1].astype(int))]
+
+    # checking the indexing of the sequence
+    first_ind = int(pro_seq_sorted[0][1])
+
+    if first_ind != 1:
+        if not silent:
+            print("*** {} used as start of the sequence indexing in the mutation file ***".format(str(first_ind)))
+            print()
+
+    # checking whether the reconstructed sequence is the same as the wt sequence
+    pro_seq_inds = pro_seq_sorted[:, 1].astype(int)
+    gap_count = 0
+    gaps = []
+    for i in range(first_ind, len(pro_seq_inds) + first_ind):
+        if i < len(pro_seq_inds) - 1:
+            if pro_seq_inds[i + 1] - pro_seq_inds[i] > 1:
+                gap_count += pro_seq_inds[i + 1] - pro_seq_inds[i] - 1
+                gaps += [np.arange(pro_seq_inds[i] - first_ind + 1, pro_seq_inds[i + 1] - first_ind)]
+                if not silent:
+                    print("*** residues between {} and {} not mutated***".format(str(pro_seq_inds[i]),
+                                                                                 str(pro_seq_inds[i + 1])))
+
+    if gap_count != len(wt_seq_cs) - len(pro_seq_sorted):
+        if not silent:
+            print("    Sequence constructed from mutations:\n   ", "".join(pro_seq_sorted[:, 0]))
+        raise ValueError("Wild type sequence doesn't match the sequence reconstructed from the mutation file")
+    elif gap_count > 0:
+        fill = pro_seq_inds.copy().astype(object)
+        offset = 0
+        for i in gaps:
+            for j in i:
+                fill = np.insert(fill, j - offset, "_")
+                offset += 1
+        print("*** sequence check passed ***")
+
+        under_fill = []
+        rec_seq = []
+        for i in fill:
+            if i == "_":
+                rec_seq += ["-"]
+                under_fill += ["*"]
+            else:
+                rec_seq += [wt_seq_cs[int(i) - 1]]
+                under_fill += [" "]
+
+        r_seq_str = "".join(rec_seq)
+        w_seq_str = "".join(wt_seq_cs)
+        uf = "".join(under_fill)
+        if not silent:
+            print("reconstructed sequence\nwild type sequence\ngap indicator\n")
+            for i in range(0, len(wt_seq_cs), 80):
+                print(r_seq_str[i:i + 80])
+                print(w_seq_str[i:i + 80])
+                print(uf[i:i + 80])
+                print()
+    else:
+        print("*** sequence check passed ***")
+
+    if save_fig is not None:
+        # histogram for how often a residue site was part of a mutation
+        fig = plt.figure(figsize=(10, 6))
+        plt.hist(x=np.asarray(pre_seq)[:, 1].astype(int), bins=np.arange(first_ind, len(pro_seq) + 1 + first_ind),
+                 color="forestgreen")
+        plt.xlabel("residue index")
+        plt.ylabel("number of mutations")
+        plt.title(name_cs)
+        plt.savefig(save_fig + "/" + "mutation_histogram_" + name_cs)
+        if plot_fig:
+            plt.show()
+    return first_ind
+
+
+def split_data(raw_data_sd, variants_sd, score_sd, number_mutations_sd, max_train_mutations, train_split, r_seed,
+               silent=False):
+    """splits data from raw_data_sd into training and test dataset\n
+        input:
+            raw_data_sd: dms data as pandas dataframe\n
+            variants_sd: name of the variants column in raw_data_sd\n
+            score_sd: name of the score column in raw_data_sd\n
+            number_mutations_sd: name of the column that stats the number of mutations per variant in raw_data_sd \n
+            max_train_mutations: maximum number of mutations per sequence to be used for training\n
+                (None to use all mutations for training) variants with mutations > max_train_mutations get stored in
+                unseen_data\n
+            train_split: how much of the dataset should be used as training data (int to specify a number of data for the
+                training dataset or a float (<=1) to specify the fraction of the dataset used as training data\n
+            r_seed: random seed for pandas random_state\n
+            silent: if True doesn't show stats during execution\n
+        :returns
+            all numpy arrays\n
+            data: variants\n
+            labels: score\n
+            mutations: number of mutations\n
+           train_data, train_labels, train_mutations\n
+           test_data, test_labels, test_mutations\n
+           if max_train_mutations is used (variants with more mutations than max_train_mutations):
+            unseen_data, unseen_labels, unseen_mutations\n """
+    vas = raw_data_sd[[variants_sd, score_sd, number_mutations_sd]]
+
+    if max_train_mutations is None:
+        if isinstance(train_split, float):
+            vas_train = vas.sample(frac=train_split, random_state=r_seed)
+            vas_test = vas.drop(vas_train.index)
+        else:
+            vas_train = vas.sample(n=train_split, random_state=r_seed)
+            vas_test = vas.drop(vas_train.index)
+        vas_train = np.asarray(vas_train)
+        vas_test = np.asarray(vas_test)
+
+        train_data = vas_train[:, 0]
+        train_labels = vas_train[:, 1]
+        train_mutations = vas_train[:, 2]
+
+        test_data = vas_test[:, 0]
+        test_labels = vas_test[:, 1]
+        test_mutations = vas_test[:, 2]
+        if not silent:
+            print("train data size:", len(train_data))
+            print("test data size:", len(test_data))
+
+        unseen_mutations = None
+        unseen_labels = None
+        unseen_data = None
+    else:
+        vs_un = vas[vas[number_mutations_sd] <= max_train_mutations]
+        vs_up = vas[vas[number_mutations_sd] > max_train_mutations]
+        if isinstance(train_split, float):
+            vs_un_train = vs_un.sample(frac=train_split, random_state=r_seed)
+            vs_un_test = vs_un.drop(vs_un_train.index)
+        else:
+            vs_un_train = vs_un.sample(n=train_split, random_state=r_seed)
+            vs_un_test = vs_un.drop(vs_un_train.index)
+
+        vs_un_train = np.asarray(vs_un_train)
+        vs_un_test = np.asarray(vs_un_test)
+        vs_up = np.asarray(vs_up)
+
+        train_data = vs_un_train[:, 0]
+        train_labels = vs_un_train[:, 1]
+        train_mutations = vs_un_train[:, 2]
+
+        test_data = vs_un_test[:, 0]
+        test_labels = vs_un_test[:, 1]
+        test_mutations = vs_un_test[:, 2]
+
+        unseen_data = vs_up[:, 0]
+        unseen_labels = vs_up[:, 1]
+        unseen_mutations = vs_up[:, 2]
+
+        if not silent:
+            print("train data size:", len(train_data))
+            print("test data size:", len(test_data))
+            print("unseen data size:", len(unseen_data))
+    return train_data, train_labels, train_mutations, test_data, test_labels, test_mutations, unseen_data, unseen_labels, unseen_mutations
+
+
+def data_coord_extraction(target_pdb_file):
+    """calculates distance between residues and builds artificial CB for GLY based on the
+        side chains of amino acids (!= GLY) before if there is an or after it if Gly is the start amino acid\n
+        No duplicated side chain entries allowed
+            input:
+                target_pdb_file: pdb file with data of protein of interest\n
+            :returns:
+                new_data: 2d list [[Atom type, Residue 3letter, ChainID, ResidueID],...]\n
+                new_coords: 2d list of corresponding coordinates to the new_data entries\n
+                split_tuples: list of tuples which indicate the start and end of a residue in the new_data and
+                new_coords lists\n"""
+    # list of all data of the entries like [[Atom type, Residue 3letter, ChainID, ResidueID],...]
+    res_data = []
+    # list of all coordinates of the entries like [[x1, y1, z1],...]
+    res_coords = []
+    # reading the pdb file
+    file = open(target_pdb_file, "r")
+    for line in file:
+        if "ATOM  " in line[:6]:
+            line = line.strip()
+            res_data += [[line[12:16].replace(" ", "").strip(), line[17:20].replace(" ", "").strip(),
+                          line[21].replace(" ", "").strip(), line[22:26].replace(" ", "").strip()]]
+            res_coords += [[line[30:38].strip(), line[38:46].strip(), line[46:54].strip()]]
+    file.close()
+
+    res_data = np.asarray(res_data)
+    res_coords = np.asarray(res_coords, dtype=float)
+
+    def art_cb(inc_bool):
+        """gets the CA and CB coordinates of the residue at inc_bool=True,computed the difference in the CA atom
+            coordinates of this residue and the Gly and uses this difference to compute the 'artificial CB' for the Gly
+            if entry for CB is duplicated for an amino acid the mean of its coordinates are used
+            input:
+                increase: residue id of the closest amino acid with a CB
+            :return
+                art_cbc: CA, CB coordinates as [[xa, ya, za]], [[xb, yb, zb]]"""
+        # data and coords of the next amino acid != GLY
+        increased_data = res_data[inc_bool]
+        increased_coords = res_coords[inc_bool]
+        # CB and CA coordinates of the next amino acid != GLY
+        next_cb = increased_coords[increased_data[:, 0] == "CB"]
+        next_ca = increased_coords[increased_data[:, 0] == "CA"]
+        # CA coords of the GLY
+        true_cac = i_coords[i_data[:, 0] == "CA"]
+        # difference in CA coordinates to compute the artificial CB for the GLY
+        delta = next_ca - true_cac
+        art_cbc = next_cb - delta
+        # print("pseudoatom tmpPoint2, resi=40, chain=ZZ, b=40, color=red, pos=", art_cbc[0].tolist())
+        return art_cbc
+
+    split_tuples = []
+    last_ind = 0
+    new_coords = []
+    new_data = []
+    # RES, CHAIN, ResNR sorted
+    residues = np.unique(res_data[:, 1:], axis=0)
+    residues = residues[np.lexsort((residues[:, 2].astype(int), residues[:, 1]))]
+    for ci, i in enumerate(residues):
+        i_bool = np.all(res_data[:, 1:] == i, axis=1)
+        i_data = res_data[i_bool]
+        i_coords = res_coords[i_bool]
+        if i_data[0][1] == "GLY":
+            # if GLY is the first amino acid or all residues so far were GLY
+            if len(new_data) == 0 or (np.all(residues[:ci, 0] == "GLY") and len(residues[:ci]) > 0):
+                # to look at next amino acid(s)
+                i_increase = 1
+                sign = 1
+            else:
+                # to look at previous amino acid(s)
+                i_increase = -1
+                sign = -1
+            # get the index of the next amino acid that is no Gly
+            while residues[ci + i_increase][0] == "GLY":
+                i_increase += 1 * sign
+            # where this data is located as boolean list
+            increase_bool = np.all(res_data[:, 1:] == residues[ci + i_increase], axis=1)
+            # artificial CB coordinates of Gly
+            cb_coords = art_cb(increase_bool)
+            new_i_coords = np.append(i_coords, cb_coords, axis=0)
+
+            data_inter = i_data[0].copy()
+            # artificial CB entry
+            data_inter[0] = "CB"
+            new_i_data = np.append(i_data, np.asarray([data_inter]), axis=0)
+            # new_i_data[:, 1] = "ALA"
+            new_coords += new_i_coords.tolist()
+            new_data += new_i_data.tolist()
+            # length of the
+            length = len(new_i_data)
+            # start and end of the residue entries
+            split_tuples += [(last_ind, last_ind + length)]
+            last_ind += length
+        else:
+            new_coords += i_coords.tolist()
+            new_data += i_data.tolist()
+            length = len(i_data)
+            split_tuples += [(last_ind, last_ind + length)]
+            last_ind += length
+    return np.asarray(new_data), np.asarray(new_coords, dtype=float), split_tuples
+
+
+def check_structure(path_to_pdb_file, comb_bool_cs, wt_seq_cs):
+    """checks whether the given wild type sequence matches the sequence in the pdb file\n
+        input:
+            path_to_pdb_file: path to used pdb file\n
+            comb_bool_cs: interacting_residues of atom_interaction_matrix_d\n
+            wt_seq_cs: wild type sequence as list eg ['A', 'V', 'L']\n
+        :return
+            None
+        """
+    if len(comb_bool_cs) != len(wt_seq_cs):
+        raise ValueError("Wild type sequence doesn't match the sequence in the pdb file (check for multimers)\n")
+    else:
+        # could be read only one time (additional input for atom_interaction_matrix(_d))
+        pdb_seq = np.unique(data_coord_extraction(path_to_pdb_file)[0][:, 1:], axis=0)
+        pdb_seq_sorted = pdb_seq[np.lexsort((pdb_seq[:, 2].astype(int), pdb_seq[:, 1]))]
+        # sequence derived from pdb file
+        pdb_seq_ol_list = np.asarray(list(map(aa_dict.get, pdb_seq_sorted[:, 0])))
+        if not np.all(np.asarray(wt_seq_cs) == pdb_seq_ol_list):
+            raise ValueError("Wild type sequence doesn't match the sequence derived from the pdb file\n")
+        else:
+            print("*** structure check passed ***")
+
+
+if __name__ == "__main__":
+    pass
---
+++ d4_stats.py
@@ -0,0 +1,172 @@
+import scipy.stats
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def validate(generator_v, model_v, history_v, name_v, max_train_mutations_v, save_fig_v=None, plot_fig=False):
+    # get loss, accuracy and history of the previous trained model and plotting it
+    test_loss, test_acc = model_v.evaluate(generator_v, verbose=2)
+    train_val = history_v.history['mae']
+    val_val = history_v.history['val_mae']
+    plt.plot(train_val, label='mae', color="forestgreen")
+    plt.plot(val_val, label='val_mae', color="firebrick")
+    plt.xlabel('Epoch')
+    plt.ylabel('mae')
+    all_vals = np.concatenate((train_val, val_val))
+    ymin = np.min(all_vals)
+    ymax = np.max(all_vals)
+    plt.ylim([ymin - ymin * 0.1, ymax + ymax * 0.1])
+    plt.legend()
+    # epoch with the best weights
+    epochs_bw = np.argmin(train_val)
+    # stats
+    val_text = name_v + "\nmax_mut_train: " + str(max_train_mutations_v) + "\nepochs for best weights: " + str(
+        epochs_bw) + "\nmae: " + str(np.around(test_loss, decimals=4))
+    plt.gcf().text(0.5, 0.9, val_text, fontsize=14)
+    plt.subplots_adjust(left=0.5)
+    if save_fig_v is not None:
+        plt.savefig(save_fig_v + "/" + "history_" + name_v)
+    if plot_fig:
+        plt.show()
+    return val_val, epochs_bw, test_loss
+
+
+def pearson_spearman(model, generator, labels):
+    """calculating the pearson r and spearman r for generator\n
+        input:
+            generator: Data generator to predict values (not shuffled)\n
+            labels: the corresponding labels for the generator\n
+        return:
+            pearson_r: Pearson’s correlation coefficient\n
+            pearson_r_p: Two-tailed p-value\n
+            spearman_r: Spearman correlation coefficient\n
+            spearman_r_p: p-value for a hypothesis test whose null hypothesis is that two sets of data are
+            uncorrelated\n
+            """
+    pred = model.predict(generator).flatten()
+    ground_truth = labels
+    pearson_r, pearson_r_p = scipy.stats.pearsonr(ground_truth.astype(float), pred.astype(float))
+    spearman_r, spearman_r_p = scipy.stats.spearmanr(ground_truth.astype(float), pred.astype(float))
+    return pearson_r, pearson_r_p, spearman_r, spearman_r_p
+
+
+def validation(model, generator, labels, v_mutations, p_name, test_num, name,
+               save_fig=None, plot_fig=False, silent=True):
+    """plot validations\n
+        input:
+            generator: data generator for predicting values\n
+            labels: the corresponding real labels to the generator\n
+            v_mutations: number of mutations per data sample in the generator\n
+            p_name: protein name
+            test_num: number of samples used for the test, name,
+               save_fig=None, plot_fig=False, silent=True
+            """
+    # predicted values, errors between prediction and label, number of mutations per label
+    pred = model.predict(generator).flatten()
+    all_errors = np.abs(pred - labels)
+    mutations = v_mutations
+
+    # sort the errors acording to the number of mutations
+    mut_sort = np.argsort(mutations)
+    mutations = np.asarray(mutations)[mut_sort]
+    all_errors = all_errors[mut_sort]
+
+    fig = plt.figure(figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=2)
+    ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2, rowspan=2)
+    ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2)
+
+    # histogram of number of each mutations present in the features used in the test
+    ax1.hist(x=mutations, bins=np.arange(1, np.max(mutations) + 1, 1), color="forestgreen")
+    ax1.set_ylabel("occurrence")
+    ax1.set_xlabel("mutations")
+    ax1.xaxis.set_label_position("top")
+    ax1.set_xlim([np.min(mutations), np.max(mutations)])
+    ax1.tick_params(top=True, bottom=False, labelbottom=False, labeltop=True)
+    ax1.set_xticks(np.arange(1, np.max(mutations) + 1, 1))
+
+    c = ["firebrick", "black"]
+    cc = []
+    for i in range(len(mutations)):
+        if i % 2 == 0:
+            cc += [c[0]]
+        else:
+            cc += [c[1]]
+    # amount of values for each number of mutation
+    _, w = np.unique(mutations, return_counts=True)
+
+    wx = []
+    prev_ind = 0
+    mean_error_per_mutations = []
+    for i in range(len(w)):
+        if i == 0:
+            wx += [0]
+        else:
+            wx += [wx[-1] + w[i - 1]]
+        # mean of all errors when i number of mutations are present
+        # value is as often in mean_error_per_mutations as often i number of mutations are present
+        mean_error_per_mutations += [np.mean(all_errors[prev_ind:prev_ind + int(w[i])])] * int(w[i])
+        prev_ind += int(w[i])
+    # which errors origin from how many mutations
+    ax2.bar(x=wx, width=w, height=[np.max(all_errors)] * len(w), color=cc, align="edge", alpha=0.25)
+    # errors of each prediction illustrated as point
+    ax2.scatter(np.arange(len(all_errors)), all_errors, color="yellowgreen", label="error", s=3)
+    # ax2.plot(np.arange(len(mutations)), np.asarray(mutations) / 10, color="firebrick")
+    # mean error of all errors originating from certain number of mutations
+    ax2.plot(np.arange(len(mutations)), np.asarray(mean_error_per_mutations), color="firebrick",
+             label="mean error per mutation")
+    ax2.set_xlabel("sample index")
+    ax2.set_ylabel("absolute error")
+    ax2.legend(loc="upper right")
+
+    # histogram of how often an error of magnitude "y" occurred
+    ax3.hist(all_errors, bins=np.arange(0, np.max(all_errors), 0.01), orientation="horizontal", color="forestgreen")
+    ax3.set_xlabel("occurrence")
+    ax3.tick_params(left=False, labelleft=False)
+    plt.tight_layout()
+
+    test_pearson_r, test_pearson_rp = scipy.stats.pearsonr(labels.astype(float), pred.astype(float))
+    test_spearman_r, test_spearman_rp = scipy.stats.spearmanr(labels.astype(float), pred.astype(float))
+
+    test_text = p_name + "\nsample number: " + str(test_num) + "\nmean_error: " + str(
+        np.round(np.mean(all_errors), decimals=4)) + "\npearson r: " + str(
+        np.around(test_pearson_r, 4)) + "\nspearman r: " + str(np.around(test_spearman_r, 4))
+    plt.gcf().text(0.7, 0.8, test_text, fontsize=14)
+    if save_fig is not None:
+        plt.savefig(save_fig + "/" + "test_" + name)
+    if plot_fig:
+        plt.show()
+
+    # boxplot of errors per number of mutations
+    fig = plt.figure(figsize=(10, 10))
+    boxes = []
+    for i in range(1, np.max(mutations) + 1):
+        i_bool = mutations == i
+        boxes += [all_errors[i_bool].tolist()]
+    plt.boxplot(boxes)
+    plt.xticks(range(1, np.max(mutations) + 1))
+    if save_fig is not None:
+        plt.savefig(save_fig + "/" + "boxplot_" + name)
+    plt.ylabel("error")
+    plt.xlabel("number of mutations")
+    if plot_fig:
+        plt.show()
+    if not silent:
+        print("mean error:", np.mean(all_errors))
+
+    # correlation scatter plot
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(labels, pred, color="forestgreen", s=3)
+    tr = max(np.max(pred), np.max(labels))
+    bl = min(np.min(pred), np.min(labels))
+    plt.xlabel("true score")
+    plt.ylabel("predicted score")
+    plt.plot([tr, bl], [tr, bl], color="firebrick")
+    if save_fig is not None:
+        plt.savefig(save_fig + "/" + "correlation_" + name)
+    if plot_fig:
+        plt.show()
+
+
+if __name__ == "__main__":
+    pass
