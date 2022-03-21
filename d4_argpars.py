0b77990ab553bb06a6880315a9f9bed77e00197f
gwirn <71886945+gwirn@users.noreply.github.com>
2022-03-21 07:28:00 +0100

    new description of arguments and simplification of the file

--- d4_argpars.py
+++ d4_argpars.py
@@ -30,65 +30,103 @@ def arg_dict(p_dir=""):
     for ci, i in enumerate(pos_optimizer):
         opt_str += [str(ci) + " " + str(i).split(" ")[1] + "\n"]
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--protein_name", type=str, required=False, default=None,
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-pn", "--protein_name", type=str, required=False, default=None,
                         help="str: name of the protein in the protein settings file")
-    parser.add_argument("--transfer_conv_weights", type=str, required=False, default=None,
+    parser.add_argument("-tw", "--transfer_conv_weights", type=str, required=False, default=None,
                         help="str: file path to a suitable trained network to transfer its convolution layer weights "
                              "to the new model")
-    parser.add_argument("--train_conv_layers", type=bool, required=False, default=False,
-                        help="bool: If True also the convolution layer get trained else they are set to"
-                             " trainable=False")
-    parser.add_argument("--training_epochs", type=int, required=False, default=100,
+    parser.add_argument("-tl", "--train_conv_layers", action="store_true",
+                        help="set flag to train the convolution layer else they are set to trainable=False")
+    parser.add_argument("-te", "--training_epochs", type=int, required=False, default=100,
                         help="int: number of training epochs")
-    parser.add_argument("--es_patience", type=int, required=False, default=20, help="es_patience")
-    parser.add_argument("--batch_size", type=int, required=False, default=64, help="batch_size")
-    parser.add_argument("--learning_rate", type=float, required=False, default=0.001, help="learning_rate")
-    parser.add_argument("--split0", type=float, required=False, default=0.8, help="split 0")
-    parser.add_argument("--split1", type=float, required=False, default=0.2, help="split 1")
-    parser.add_argument("--split2", type=float, required=False, default=0.0, help="split 2")
-    parser.add_argument("--architecture", type=int, required=False, default=0, help=" ".join(model_str))
-    parser.add_argument("--save_model", type=bool, required=False, default=False, help="save_model")
-    parser.add_argument("--settings_test", type=bool, required=False, default=False, help="settings_test")
-    parser.add_argument("--dist_thr", type=float, required=False, default=20, help="dist_thr")
-    parser.add_argument("--channel_num", type=int, required=False, default=6, help="channel_num")
-    parser.add_argument("--load_trained_weights_path", type=str, required=False, default=None,
-                        help="load_trained_weights_path")
-    parser.add_argument("--load_trained_model_path", type=str, required=False, default=None,
-                        help="load_trained_model_path")
-    parser.add_argument("--max_train_mutations", type=int, required=False, default=None, help="max_train_mutations")
-    parser.add_argument("--test_num", type=int, required=False, default=5000, help="test_num")
-    parser.add_argument("--random_seed", type=int, required=False, default=None, help="random_seed")
-    parser.add_argument("--save_figures", type=int, required=False, default=None, help="save_figures")
-    parser.add_argument("--show_figures", type=bool, required=False, default=False, help="show_figures")
-    parser.add_argument("--silent_execution", type=bool, required=False, default=True, help="silent_execution")
-    parser.add_argument("--extensive_test", type=bool, required=False, default=False, help="extensive_test")
-    parser.add_argument("--deploy_early_stop", type=bool, required=False, default=True, help="deploy_early_stop")
-    parser.add_argument("--no_nan", type=bool, required=False, default=True, help="no_nan")
-    parser.add_argument("--write_temp", type=bool, required=False, default=False, help="write_temp")
-    parser.add_argument("--optimizer", type=int, required=False, default=0, help="\n".join(opt_str))
-    parser.add_argument("--p_dir", type=str, required=False, default=p_dir, help="p_dir")
-    parser.add_argument("--split_file_creation", type=bool, required=False, default=False, help="split_file_creation")
-    parser.add_argument("--use_split_file", type=str, required=False, default=None, help="use_split_file")
-
-    parser.add_argument("--number_mutations", type=str, required=False, default=None,
-                        help="number_mutations column name")
-    parser.add_argument("--variants", type=str, required=False, default=None, help="variants column name")
-    parser.add_argument("--score", type=str, required=False, default=None, help="score column name")
-    parser.add_argument("--wt_seq", type=str, required=False, default=None, help="wt_seq")
-    parser.add_argument("--first_ind", type=str, required=False, default=None, help="first residue index")
-    parser.add_argument("--tsv_filepath", type=str, required=False, default=None, help="tsv_filepath")
-    parser.add_argument("--pdb_filepath", type=str, required=False, default=None, help="pdb_filepath")
-
+    parser.add_argument("-ep", "--es_patience", type=int, required=False, default=20,
+                        help="number of epochs the model can try to decrease its es_monitor value for at least "
+                             "min_delta before")
+    parser.add_argument("-bs", "--batch_size", type=int, required=False, default=64,
+                        help="after how many samples the gradient gets updated")
+    parser.add_argument("-lr", "--learning_rate", type=float, required=False, default=0.001,
+                        help="how much the weights can change during an update")
+    parser.add_argument("-s0", "--split0", type=float, required=False, default=0.8,
+                        help="size of the training data set - can be either a fraction or a number of samples")
+    parser.add_argument("-s1", "--split1", type=float, required=False, default=0.2,
+                        help="size of the tune data set - can be either a fraction or a number of samples")
+    parser.add_argument("-s2", "--split2", type=float, required=False, default=0.0,
+                        help="size of the test data set - can be either a fraction or a number of samples")
+    parser.add_argument("-a", "--architecture", type=int, required=False, default=0,
+                        help="input number of model that should be used"+" ".join(model_str))
+    parser.add_argument("-sm", "--save_model", action="store_true",
+                        help="set flag to save the model after training")
+    parser.add_argument("-st", "--settings_test", action="store_true",
+                        help="set flag doesn't train the model and only executes everything of the function that is"
+                             " before model.fit()")
+    parser.add_argument("-dt", "--dist_thr", type=float, required=False, default=20,
+                        help="threshold distances between any side chain atom to count as interacting")
+    parser.add_argument("-cn", "--channel_num", type=int, required=False, default=6,
+                        help="number of channels = number of matrices used")
+    parser.add_argument("-lw", "--load_trained_weights_path", type=str, required=False, default=None,
+                        help="path to model of who's weights should be used None if it shouldn't be used")
+    parser.add_argument("-lm", "--load_trained_model_path", type=str, required=False, default=None,
+                        help="path to an already trained model or None to not load a model")
+    parser.add_argument("-mm", "--max_train_mutations", type=int, required=False, default=None,
+                        help="int specifying maximum number of mutations per sequence to be used for training -"
+                             "None to use all mutations for training")
+    parser.add_argument("-tn", "--test_num", type=int, required=False, default=5000,
+                        help="number of samples for the test after the model was trained")
+    parser.add_argument("-rs", "--random_seed", type=int, required=False, default=None,
+                        help="numpy and tensorflow random seed")
+    parser.add_argument("-sf", "--save_figures", type=int, required=False, default=None,
+                        help="str specifying the file path where the figures should be stored or "
+                             "None to not save figures")
+    parser.add_argument("-pf", "--show_figures", type=bool, required=False, default=False,
+                        help="set flag to show figures")
+    parser.add_argument("-se", "--silent_execution", action="store_false",
+                        help="set flag to print stats in the terminal")
+    parser.add_argument("-et", "--extensive_test", action="store_true",
+                        help="set flag so more test are done and more detailed plots are created")
+    parser.add_argument("-es", "--deploy_early_stop", action="store_false",
+                        help="set flag to early stop during training should be enabled")
+    parser.add_argument("-nn", "--no_nan", action="store_false",
+                        help="set flag to terminate training on nan")
+    parser.add_argument("-wf", "--write_temp", action="store_true",
+                        help="set flag to write mae, loss and time per epoch of each epoch to the temp.csv in "
+                             "result_files")
+    parser.add_argument("-o", "--optimizer", type=int, required=False, default=0,
+                        help="input int of which optimizer to use"+"\n".join(opt_str))
+    parser.add_argument("-pd", "--p_dir", type=str, required=False, default=p_dir,
+                        help="path to the projects content root - default=''")
+    parser.add_argument("-fc", "--split_file_creation", action="store_true",
+                        help="set flag to create directory containing train.txt, tune.txt"
+                             " and test.txt files that store the indices of the rows used from the tsv file "
+                             "during training, validating and testing")
+    parser.add_argument("-uf", "--use_split_file", type=str, required=False, default=None,
+                        help="if not None this needs the file_path to a directory containing "
+                             "splits specifying the 'train', 'tune', 'test' indices - these files need to be named"
+                             " 'train.txt', 'tune.txt' and 'test.txt' otherwise splits of the tsv file according to "
+                             "split_def will be used")
+    parser.add_argument("-nm", "--number_mutations", type=str, required=False, default=None,
+                        help="how the number of mutations column is named - needed when protein_name is not defined")
+    parser.add_argument("-v", "--variants", type=str, required=False, default=None,
+                        help="name of the variant column - needed when protein_name is not defined")
+    parser.add_argument("-s", "--score", type=str, required=False, default=None,
+                        help="name of the score column - needed when protein_name is not defined")
+    parser.add_argument("-wt", "--wt_seq", type=str, required=False, default=None,
+                        help="wt sequence of the protein of interest eg. 'AVL...' - "
+                             "needed when protein_name is not defined - needed when protein_name is not defined")
+    parser.add_argument("-fi", "--first_ind", type=str, required=False, default=None,
+                        help="offset of the start of the sequence (when sequence doesn't start with residue 0) "
+                             "- needed when protein_name is not defined")
+    parser.add_argument("-tp", "--tsv_filepath", type=str, required=False, default=None,
+                        help="path to tsv file containing dms data of the protein of interest - needed when tsv file is"
+                             "not stored in /datasets or protein_name is not given or the file is not named "
+                             "protein_name.tsv")
+    parser.add_argument("-pp", "--pdb_filepath", type=str, required=False, default=None,
+                        help="path to pdb file containing the structure data of the protein of interest - "
+                             "needed when pdb file is not stored in /datasets or protein_name is not given or"
+                             " the file is not named protein_name.pdb")
+    # parser.add_argument("-pf", "--pdb_filepath", action="store_false")  # or store_true to have false as default
     args = parser.parse_args()
     protein_name = args.protein_name
-    p_dir_ex = args.p_dir
-    transfer_conv_weights_ex = args.transfer_conv_weights
-    train_conv_layers_ex = args.train_conv_layers
-    training_epochs_ex = args.training_epochs
-    es_patience_ex = args.es_patience
-    batch_size_ex = args.batch_size
-    lr_ex = args.learning_rate
 
     if args.split2 > 0.:
         split_def_ex = [args.split0, args.split1, args.split2]
@@ -96,23 +134,6 @@ def arg_dict(p_dir=""):
         split_def_ex = [args.split0, args.split1]
 
     architecture = pos_models[args.architecture]
-    save_model_ex = args.save_model
-    settings_test_ex = args.settings_test
-    dist_thr_ex = args.dist_thr
-    channel_num_ex = args.channel_num
-    load_trained_weights_ex = args.load_trained_weights_path
-    load_trained_model_ex = args.load_trained_model_path
-    max_train_mutations_ex = args.max_train_mutations
-    test_num_ex = args.test_num
-    r_seed_ex = args.random_seed
-    save_fig_ex = args.save_figures
-    show_fig_ex = args.show_figures
-    silent_ex = args.silent_execution
-    ex_test = args.extensive_test
-    deploy_early_stop_ex = args.deploy_early_stop
-    no_nan_ex = args.no_nan
-    write_temp_ex = args.write_temp
-    architecture_name = architecture.__code__.co_name
 
     # check when protein name is None
     nm = args.number_mutations
@@ -162,30 +183,30 @@ def arg_dict(p_dir=""):
     split_file_creation_ex = args.split_file_creation
     use_split_file_ex = args.use_split_file
 
-    d = {"p_dir": p_dir_ex,
-         "transfer_conv_weights": transfer_conv_weights_ex,
-         "train_conv_layers": train_conv_layers_ex,
-         "training_epochs": training_epochs_ex,
-         "es_patience": es_patience_ex,
-         "batch_size": batch_size_ex,
-         "lr": lr_ex,
+    d = {"p_dir": args.p_dir,
+         "transfer_conv_weights": args.transfer_conv_weights,
+         "train_conv_layers": args.train_conv_layers,
+         "training_epochs": args.training_epochs,
+         "es_patience": args.es_patience,
+         "batch_size": args.batch_size,
+         "lr": args.learning_rate,
          "split_def": split_def_ex,
-         "save_model": save_model_ex,
-         "settings_test": settings_test_ex,
-         "dist_thr": dist_thr_ex,
-         "channel_num": channel_num_ex,
-         "load_trained_weights": load_trained_weights_ex,
-         "load_trained_model": load_trained_model_ex,
-         "max_train_mutations": max_train_mutations_ex,
-         "test_num": test_num_ex,
-         "r_seed": r_seed_ex,
-         "save_fig": save_fig_ex,
-         "show_fig": show_fig_ex,
-         "silent": silent_ex,
-         "extensive_test": ex_test,
-         "deploy_early_stop": deploy_early_stop_ex,
-         "no_nan": no_nan_ex,
-         "write_temp": write_temp_ex,
+         "save_model": args.save_model,
+         "settings_test": args.settings_test,
+         "dist_thr": args.dist_thr,
+         "channel_num": args.channel_num,
+         "load_trained_weights": args.load_trained_weights_path,
+         "load_trained_model": args.load_trained_model_path,
+         "max_train_mutations": args.max_train_mutations,
+         "test_num": args.test_num,
+         "r_seed": args.random_seed,
+         "save_fig": args.save_figures,
+         "show_fig": args.show_figures,
+         "silent": args.silent_execution,
+         "extensive_test": args.extensive_test,
+         "deploy_early_stop": args.deploy_early_stop,
+         "no_nan": args.no_nan,
+         "write_temp": args.write_temp,
          "number_mutations": number_mutations_ex,
          "variants": variants_ex,
          "score": score_ex,
@@ -193,7 +214,7 @@ def arg_dict(p_dir=""):
          "first_ind": first_ind_ex,
          "tsv_file": tsv_ex,
          "pdb_file": pdb_ex,
-         "architecture_name": architecture_name,
+         "architecture_name": architecture.__code__.co_name,
          "optimizer": opt,
          "model_to_use": architecture,
          "split_file_creation": split_file_creation_ex,
