6d6926bf42e572838177624c6072df591ce47757
gwirn <71886945+gwirn@users.noreply.github.com>
2022-03-21 07:25:16 +0100

    small rearrangements

--- d4_generation.py
+++ d4_generation.py
@@ -1,14 +1,16 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 from timeit import default_timer as timer
 from datetime import datetime
-
+import time
 import os
 import sys
 import warnings
 import logging
 import gc
+import random
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 from d4_utils import create_folder, log_file, hydrophobicity, h_bonding, charge, sasa, side_chain_length, clear_log
 from d4_stats import validate, validation, pearson_spearman
@@ -17,6 +19,7 @@ from d4_interactions import atom_interaction_matrix_d, hydrophobicity_matrix, hb
     interaction_area, clashes, mutate_sequences, check_structure
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['PYTHONHASHSEES'] = str(0)
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -131,10 +134,10 @@ class DataGenerator(keras.utils.Sequence):
     - dist_mat 2D ndarray of floats\n
       ture distances between all residues
     - dist_th
-      maximum distance to be counted as interaction
+      maximum distance for residues to be counted as interaction
     - shuffle: bool, (optional - default True)\n
       if True data gets shuffled after every epoch\n
-    - train: bool, (optional - default True\n
+    - train: bool, (optional - default True)\n
       if True Generator returns features and labels (use turing training) else only features\n
     """
 
@@ -234,10 +237,18 @@ class SaveToFile(keras.callbacks.Callback):
 
     def __init__(self, filepath):
         self.filepath = filepath
+        self.start_time_epoch = 0.
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.start_time_epoch = time.time()
 
     def on_epoch_end(self, epoch, logs=None):
-        log_string = "{},{:0.4f},{:0.4f}".format(str(epoch), logs["loss"], logs["val_loss"])
-        log_file(self.filepath, write_str=log_string, optional_header="epoch,loss,mae,val_loss,val_mae")
+        log_string = "{},{:0.4f},{:0.4f},{:0.4f}".format(str(epoch), logs["loss"], logs["val_loss"],
+                                                         time.time() - self.start_time_epoch)
+        log_file(self.filepath, write_str=log_string, optional_header="epoch,loss,mae,val_loss,val_mae,sec_per_epoch")
+
+    def on_train_end(self, logs=None):
+        log_file(self.filepath, write_str="Finished training")
 
 
 class CustomPrint(keras.callbacks.Callback):
@@ -260,15 +271,24 @@ class CustomPrint(keras.callbacks.Callback):
         self.latest_loss = 0.
         self.latest_val_loss = 0.
         self.epoch_stat_print = epoch_stat_print
+        self.start_time_epoch = 0.
+        self.start_time_training = 0.
+
+    def on_train_begin(self, logs=None):
+        self.start_time_training = time.time()
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.start_time_epoch = time.time()
 
     def on_epoch_end(self, epoch, logs=None):
         cur_loss = logs["loss"]
         cur_val_loss = logs["val_loss"]
 
         if epoch % self.epoch_print == 0:
-            print("Epoch {} -- loss: {:0.4f}   val loss: {:0.4f}  --  loss change: {:0.4f}   val loss change: {:0.4f}\n"
-                  .format(str(epoch), cur_loss, cur_val_loss, cur_loss - self.latest_loss,
-                          cur_val_loss - self.latest_val_loss))
+            print("E {} - loss: {:0.4f}  val_loss: {:0.4f} - loss change: {:0.4f}  val_loss change: {:0.4f} - "
+                  "seconds per epoch: {:0.4f}\n".format(str(epoch), cur_loss, cur_val_loss, cur_loss - self.latest_loss,
+                                                        cur_val_loss - self.latest_val_loss,
+                                                        time.time() - self.start_time_epoch))
 
         self.latest_loss = cur_loss
         self.latest_val_loss = cur_val_loss
@@ -286,13 +306,17 @@ class CustomPrint(keras.callbacks.Callback):
                 dp = (d / self.best_val_loss) * 100
             else:
                 dp = np.nan
-            print("Best train epoch: {}\nBest validation epoch: {}\ndelta: {:0.4f} (equals {:0.2f}% of val loss)\n"
-                  .format(str(self.bl_epoch), str(self.bvl_epoch), d, dp))
+            d_cl = cur_loss - self.best_loss
+            d_cvl = cur_val_loss - self.best_val_loss
+            print("Best train epoch: {}\nBest validation epoch: {}\ndelta: {:0.4f} (equals {:0.2f}% of val_loss)\n"
+                  "difference to best loss: {:0.4f}\ndifference to best val_loss: {:0.4f}\n"
+                  .format(str(self.bl_epoch), str(self.bvl_epoch), d, dp, d_cl, d_cvl))
 
     def on_train_end(self, logs=None):
-        print("overall best epochs")
-        print("best training epoch: {} with a loss of {:0.4f}".format(str(self.bl_epoch), self.best_loss))
-        print("best validation epoch: {} with a loss of {:0.4f}\n".format(str(self.bvl_epoch), self.best_val_loss))
+        print("Overall best epoch stats")
+        print("Best training epoch: {} with a loss of {:0.4f}".format(str(self.bl_epoch), self.best_loss))
+        print("Best validation epoch: {} with a loss of {:0.4f}".format(str(self.bvl_epoch), self.best_val_loss))
+        print("Total training time in minutes: {:0.1f}\n".format((time.time() - self.start_time_training) / 60))
 
 
 class ClearMemory(keras.callbacks.Callback):
@@ -321,14 +345,14 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
       name of the architecture\n
     - model_to_use: function object\n
       function that returns the model\n
-    - optimizer: Optimizer object
-      optimizer to be used\n
+    - optimizer: Optimizer object\n
+      keras optimizer to be used\n
     - tsv_file: str\n
       path to tsv file containing dms data of the protein of interest\n
     - pdb_file: str\n
       path to pdb file containing the structure\n
     - wt_seq: str\n
-      wt sequence of the protein of interest\n
+      wt sequence of the protein of interest eg. 'AVL...'\n
     - number_mutations: str\n
       how the number of mutations column is named\n
     - variants:str\n
@@ -349,7 +373,7 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
     - first_ind: int\n
       offset of the start of the sequence (when sequence doesn't start with residue 0)
     - r_seed: None, int, (optional - default None)\n
-      numpy random seed\n
+      numpy and tensorflow random seed\n
     - deploy_early_stop: bool, (optional - default True)\n
       whether early stop during training should be enabled (Ture) or not (False)\n
             - es_monitor: str, (optional - default 'val_loss')\n
@@ -357,7 +381,8 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
             - es_min_d: float, (optional - default 0.01)\n
               min_delta - min difference in es_monitor to not stop training\n
             - es_patience: int, (optional - default 20)\n
-              number of epochs the model can try to get a es_monitor > es_min_d before stopping\n
+              number of epochs the model can try to decrease its es_monitor value for at least min_delta before
+              stopping\n
             - es_mode: str, (optional - default 'auto')\n
               direction of quantity monitored in es_monitor\n
             - es_restore_bw: bool, (optional - default True)\n
@@ -365,27 +390,28 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
     - batch_size: int, (optional - default 64)\n
       after how many samples the gradient gets updated\n
     - load_trained_model: str or None, (optional - default None)\n
-      path to an already trained model\n
-    - save_fig: None, (optional - default None)\n
-      any to save figures in result_path\n
+      path to an already trained model or None to not load a model\n
+    - save_fig: str or None, (optional - default None)\n
+            - None to not save figures\n
+            - str specifying the file path where the figures should be stored\n
     - show_fig: bool, (optional - default False)\n
       True to show figures\n
     - write_to_log: bool, (optional - default True)\n
       if True writes all parameters used in the log file - !should be always enabled!\n
     - silent: bool, (optional - default False)\n
       True to print stats in the terminal\n
-    - extensive_test: bool, (optional - default False):
-      if True more test are done and more detailed plots are created
+    - extensive_test: bool, (optional - default False)\n
+      if True more test are done and more detailed plots are created\n
     - save_model: bool, (optional - default False)\n
-      Ture to saves the model\n
+      True to save the model after training\n
     - load_trained_weights: str or None, (optional - default None)\n
       path to model of who's weights should be used None if it shouldn't be used\n
     - no_nan: bool, (optional - default True)\n
       True terminates training on nan\n
     - settings_test: bool, (optional - default False)\n
-      Ture doesn't train the model and only executes everything of the function that is before fit
+      Ture doesn't train the model and only executes everything of the function that is before model.fit()
     - p_dir: str, (optional - default '')\n
-      path to where the results, figures and log_file should be saved
+      path to the projects content root\n
     - split_def: list of int/float or None, (optional - default None)\n
       specifies the split for train, tune, test indices\n
             - float specifies fractions of the whole dataset
@@ -394,24 +420,26 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
             - int specifies the different number of samples per dataset
               eg [50,50,100] leads to a train and a tune dataset with 50 entries each and a test dataset of 100
               if the whole dataset contains 200 entries\n
+            - None uses [0.8, 0.15, 0.05] as split\n
     - validate_training: bool, (optional - default False)\n
       if True validation of the training will be performed
     - lr: float, (optional - default 0.001)\n
-      learning rate (how much the weights can change during an update)
+      learning rate (how much the weights can change during an update)\n
     - transfer_conv_weights: str or None, (optional - default None)\n
       path to model who's weights of it's convolution layers should be used for transfer learning (needs to have the
-      same architecture for the convolution part as the newly build model (model_to_use)\n
+      same architecture for the convolution part as the newly build model (model_to_use) or None to not transfer
+      weights\n
     - train_conv_layers: bool, (optional - default False)\n
-      if True convolution layers are trainable - only applies whe transfer_conv_weights is not None\n
+      if True convolution layers are trainable - only applies when transfer_conv_weights is not None\n
     - write_temp: bool, (optional - default False)\n
-      if True writes mae and loss of each epoch to the temp.csv in result_files
+      if True writes mae, loss and time per epoch of each epoch to the temp.csv in result_files\n
     - split_file_creation: bool, (optional - default False)\n
       if True creates a directory containing train.txt, tune.txt and test.txt files that store the indices of the
-      rows used from the tsv file\n
+      rows used from the tsv file during training, validating and testing\n
     - use_split_file: None or str, (optional - default None)\n
       if not None this needs the file_path to a directory containing splits specifying
       the 'train', 'tune', 'test' indices - these files need to be named 'train.txt', 'tune.txt' and 'test.txt'
-      otherwise splits according to split_def will be used\n
+      otherwise splits of the tsv file according to split_def will be used\n
     :return\n
         None\n
     """
@@ -438,7 +466,7 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
         name = "{}_{}".format(p_name, time_)
         print(name)
 
-        # clear temp file from previous content or creates it if it doesn't exist
+        # clear temp file from previous content or create it if it doesn't exist
         clear_log(temp_path, name + "\n")
 
         # clear error.log from previous run
@@ -453,6 +481,7 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
         if r_seed is not None:
             np.random.seed(r_seed)
             tf.random.set_seed(r_seed)
+            random.seed(r_seed)
 
         # creates a directory where plots will be saved
         if save_fig is not None:
@@ -491,9 +520,24 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
                               test_split=ind_dict["test"])
 
         # data to train on
+        # variants
         train_data = data_dict["train_data"]
+        # their respective score
         train_labels = data_dict["train_labels"]
+        # number of mutations per variant
         train_mutations = data_dict["train_mutations"]
+
+        # -------------------------------------------------------------------
+        """
+        from test import augment
+        d, l, m = augment(train_data, train_labels, train_mutations, runs=4)
+        
+        train_data = np.concatenate((train_data, d))
+        train_labels = np.concatenate((train_labels, l))
+        train_mutations = np.concatenate((train_mutations, m))
+        print("new train split size:", len(train_data))
+        """
+        # -------------------------------------------------------------------
 
         # data to validate during training
         test_data = data_dict["tune_data"]
@@ -504,6 +548,24 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
         unseen_data = data_dict["test_data"]
         unseen_labels = data_dict["test_labels"]
         unseen_mutations = data_dict["test_mutations"]
+
+        # create test data for the test_generator
+        if len(unseen_mutations) > 0:
+            if test_num > len(unseen_data):
+                test_num = len(unseen_data)
+            pos_test_inds = np.arange(len(unseen_data))
+            test_inds = np.random.choice(pos_test_inds, size=test_num, replace=False)
+            t_data = unseen_data[test_inds]
+            t_labels = unseen_labels[test_inds]
+            t_mutations = unseen_mutations[test_inds]
+        else:
+            if test_num > len(test_data):
+                test_num = len(test_data)
+            pos_test_inds = np.arange(len(test_data))
+            test_inds = np.random.choice(pos_test_inds, size=test_num, replace=False)
+            t_data = test_data[test_inds]
+            t_labels = test_labels[test_inds]
+            t_mutations = test_mutations[test_inds]
 
         # possible values and encoded wt_seq (based on different properties) for the DataGenerator
         hm_pos_vals, ch_good_vals, ch_mid_vals, ch_bad_vals, hp_norm, ia_norm, hm_converted, hp_converted, \
@@ -580,7 +642,7 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
         # custom stats print
         all_callbacks += [CustomPrint(epoch_print=1, epoch_stat_print=10)]
 
-        # parameters for the DatGenerator
+        # parameters for the DataGenerator
         params = {'interaction_matrix': comb_bool,
                   'dim': comb_bool.shape,
                   'n_channels': channel_num,
@@ -604,28 +666,6 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
                   'dist_th': dist_thr,
                   'shuffle': True,
                   'train': True}
-
-        # DataGenerator for training and the validation during training
-        training_generator = DataGenerator(train_data, train_labels, **params)
-        validation_generator = DataGenerator(test_data, test_labels, **params)
-
-        # create test data for the test_generator
-        if len(unseen_mutations) > 0:
-            if test_num > len(unseen_data):
-                test_num = len(unseen_data)
-            pos_test_inds = np.arange(len(unseen_data))
-            test_inds = np.random.choice(pos_test_inds, size=test_num, replace=False)
-            t_data = unseen_data[test_inds]
-            t_labels = unseen_labels[test_inds]
-            t_mutations = unseen_mutations[test_inds]
-        else:
-            if test_num > len(test_data):
-                test_num = len(test_data)
-            pos_test_inds = np.arange(len(test_data))
-            test_inds = np.random.choice(pos_test_inds, size=test_num, replace=False)
-            t_data = test_data[test_inds]
-            t_labels = test_labels[test_inds]
-            t_mutations = test_mutations[test_inds]
 
         test_params = {'interaction_matrix': comb_bool,
                        'dim': comb_bool.shape,
@@ -651,6 +691,9 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
                        'shuffle': False,
                        'train': False}
 
+        # DataGenerator for training and the validation during training
+        training_generator = DataGenerator(train_data, train_labels, **params)
+        validation_generator = DataGenerator(test_data, test_labels, **params)
         test_generator = DataGenerator(t_data, np.zeros(len(t_labels)), **test_params)
 
         if not settings_test:
@@ -693,12 +736,13 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
 
             # calculating pearsons' r and spearman r for the test dataset
             try:
-                mae, pearsonr, pp, spearmanr, sp = pearson_spearman(model, test_generator, t_labels)
+                mae, mse, pearsonr, pp, spearmanr, sp = pearson_spearman(model, test_generator, t_labels)
             except ValueError:
-                mae, pearsonr, pp, spearmanr, sp = "nan", "nan", "nan", "nan", "nan"
+                mae, mse, pearsonr, pp, spearmanr, sp = "nan", "nan", "nan", "nan", "nan", "nan"
 
-            print("{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n"
-                  .format("MAE", mae, "PearsonR", pearsonr, "PearsonP", pp, "SpearmanR", spearmanr, "SpearmanP", sp))
+            print("{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n{:<12s}{:0.4f}\n"
+                  .format("MAE", mae, "MSE", mse, "PearsonR", pearsonr, "PearsonP", pp, "SpearmanR", spearmanr,
+                          "SpearmanP", sp))
 
             # creating more detailed plots
             if extensive_test:
@@ -707,12 +751,13 @@ def run_all(architecture_name, model_to_use, optimizer, tsv_file, pdb_file, wt_s
 
             # data for the result file
             result_string = ",".join([name, architecture_name, str(len(train_data)), str(len(test_data)),
-                                      str(np.round(mae, 4)), str(np.round(pearsonr, 4)), str(np.round(pp, 4)),
-                                      str(np.round(spearmanr, 4)), str(np.round(sp, 4))])
+                                      str(np.round(mae, 4)), str(np.round(mse, 4)), str(np.round(pearsonr, 4)),
+                                      str(np.round(pp, 4)), str(np.round(spearmanr, 4)), str(np.round(sp, 4))])
             # writing results to the result file
             log_file(os.path.join(result_dir, "results.csv"), result_string,
-                     "name,architecture,train_data_size,test_data_size,mae,pearson_r,pearson_p,spearman_r,spearman_p")
-        
+                     "name,architecture,train_data_size,test_data_size,mae,mse,pearson_r,pearson_p,spearman_r,"
+                     "spearman_p")
+
         gc.collect()
         del model
 
