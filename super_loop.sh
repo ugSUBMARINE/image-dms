aa084048f5727f74a830ebf95d3b745afef6b2c4
gwirn <71886945+gwirn@users.noreply.github.com>
2022-07-11 12:57:23 +0200

    updated parameters

--- super_loop.sh
+++ super_loop.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-names=("pab1" "gb1" "avgfp")
-split=("first" "second" "third")
+names=("pab1") # "gb1" "avgfp")
+split=("first" "second" "third") 
 train_sizes=(50 100 250 500 1000 2000 6000)
 batch_size=32
-architecture=2
+architecture=15
+epochs=100
+channels=7
 
 for p_name in "${names[@]}"; do
   if [ $p_name == "avgfp" ]; then
     weigths="simple_model_imp_rr3/pab1_21_04_2022_123419"
   else
-    weigths="simple_model_imp_rr3/avgfp_21_04_2022_125134"
+    weigths="result_files/saved_models/pab1_07_07_2022_165023/"
+    algn_file="~//Downloads/pab1_1000_experimental.clustal"
   fi
   tsv="nononsense/nononsense_"$p_name".tsv"
   echo "$p_name"
@@ -22,7 +25,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" -wf
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" -wf -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
@@ -30,7 +33,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -wf
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -wf -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
@@ -38,7 +41,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -tl -wf
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -tl -wf -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
@@ -50,7 +53,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" -wf -da
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" -wf -da -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
@@ -58,7 +61,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -wf -da
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -wf -da -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
@@ -66,7 +69,7 @@ for p_name in "${names[@]}"; do
     for i in "${train_sizes[@]}"; do
       if [ -d "$path$i" ];
       then
-      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -te 100 -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -tl -wf -da
+      python3 -Wdefault ./d4_cmd_driver.py -pn "$p_name" -qn "$p_name" -af $algn_file -te $epochs -bs "$batch_size" -a "$architecture" -rs 1 --use_split_file "$path$i" --tsv_filepath "$tsv" --transfer_conv_weights $weigths -tl -wf -da -cn $channels
       else
       echo "Failed to locate folder $path$i"
       fi
